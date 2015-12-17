01b58047c9a38d8c4d81d0a7dfa5357b4d73978d
sameer-s <sameer.suri0@gmail.com>
2015-12-16 19:31:22 -0800

    Cleaned up code, removed unused code and imports, and added comments.

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -12,16 +12,32 @@ import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 import java.util.HashMap;
 
+/**
+ * The main class for my game. It has a number of purposes:
+ * This is the starting point for the game.
+ * Handles the init and update methods provided by the API.
+ * Handles the controllers (i.e. player controller) used in this game.
+ * @author Sameer Suri
+ */
 public class Main extends SimpleApplication
 {
+    // Holds a reference to our player controller
     private ThirdPersonCharacterControl playerController;
 
+    /**
+     * Starting point for the application.
+     * @param args The command line arguments passed
+     */
     public static void main(String... args)
-    {
+    {        
+        // Creates a new instance of our game and starts it.
         Main gp = new Main();
         gp.start();
     }
 
+    /**
+     * Called by the engine in order to start the game.
+     */
     @Override
     public void simpleInitApp()
     {
@@ -29,65 +45,91 @@ public class Main extends SimpleApplication
         setDisplayFps(false);
         setDisplayStatView(false);
         
-//        flyCam.setEnabled(false);
-
+        // Sets up the Bullet Physics Engine
         BulletAppState bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
 
+        // Loads the model used for the scene
         Spatial sceneModel = assetManager
                 .loadModel("Scenes/ManyLights/Main.scene");
+        // Scales it to make it a little short
         sceneModel.scale(1f, .5f, 1f);
+        // Generates a collider for physics collisions
         CollisionShape sceneShape = CollisionShapeFactory
                 .createMeshShape(sceneModel);
+        // Creates a control. What a control does is tell what it's controlling
+        // what exactly to do. A Rigid Body Control is very simple: it's a rigid
+        // body, meaning it does basically nothing. The rigid body is for the
+        // scene, which needs not move.
         RigidBodyControl scene = new RigidBodyControl(sceneShape, 0);
+        // Attaches the control to our model
         sceneModel.addControl(scene);
+        // Attaches the model to the root node
+        // This makes it appear in the world
         rootNode.attachChild(sceneModel);
+        // Registers the model with the Bullet Physics Engine
         bulletAppState.getPhysicsSpace().add(scene);
 
+        // Loads the model for our player
         Spatial playerModel = assetManager
                 .loadModel("Models/MainCharacterMale/MainCharacter3_2.j3o");
-        
+        // Makes some adjustment so it works properly
         playerModel.scale(2.f);
         playerModel.rotate(0f, 180f * FastMath.DEG_TO_RAD, 0f);
         playerModel.setLocalTranslation(-5f, 2f, 5f);
         
+        // Creates the chase camera. The chase camera is a camera which rotates
+        // and zooms around the player. There are some configuration changes
+        // I made, which I detail below:
         ChaseCamera chaseCam = new ChaseCamera(cam, playerModel, inputManager);
-        chaseCam.setHideCursorOnRotate(true);
+        // By default, you have to push down a mouse button to rotate the chase cam. This disables that.
         chaseCam.setDragToRotate(false);
-        chaseCam.setSmoothMotion(true);
+        // By default, it looks at the player model's (0,0,0), which is at its feet. This looks a bit higher.
         chaseCam.setLookAtOffset(new Vector3f(0, 1f, 0));
+        // This keeps the camera a bit closer to the player than the default. This can be changed by the scroll wheel (on the mouse).
         chaseCam.setDefaultDistance(7f);
-        chaseCam.setMaxDistance(8f);
-        chaseCam.setMinDistance(6f);
-        chaseCam.setUpVector(Vector3f.UNIT_Y);
-//        chaseCam.setChasingSensitivity(5f);
-        chaseCam.setTrailingEnabled(false);
+        // Speeds up the rotation, as the default is quite slow.
         chaseCam.setRotationSpeed(2f);
         
+        // Maps the names that the Third Person Character Controller class uses
+        // for animations to that the model uses.
         HashMap<String, String> anims = new HashMap<>();
         anims.put("Idle", "Idle");
         anims.put("Move", "Running");
                
-        playerController = new ThirdPersonCharacterControl(inputManager, anims, playerModel);
-        playerController.cam = cam;
-        //playerController.setCamera(cam);
+        // Creates our new character controller, passing in a few necessary parameters. 
+        playerController = new ThirdPersonCharacterControl(inputManager, anims, playerModel, cam);
+        // Attaches the control to the player model
         playerModel.addControl(playerController);
         
+        // Attaches the model to the root node
+        // This makes it appear in the world
         rootNode.attachChild(playerModel);
+        // Registers the model with the Bullet Physics Engine
         bulletAppState.getPhysicsSpace().add(playerModel);
-
         
+        // Creates a sun (a light) so that the player can see.
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(-.1f, -.7f, -1f));
         rootNode.addLight(sun);
     }
 
+    /**
+     * Called in a loop by the engine to allow the game to make changes.
+     * @param tpf "Time per frame"; the amount of time taken between the last update cycle and this one
+     */
     @Override
     public void simpleUpdate(float tpf)
     {
+        // Updates the player controller, allowing it to move the player and handle animation changes.
         playerController.update(tpf);
     }
 
+    /**
+     * A simple logger. Will be removed.
+     * @param tag A tag to log the data with
+     * @param val The data
+     */
     @SuppressWarnings("unused")
     private void log(Object tag, Object val)
     {
--- src/mygame/ThirdPersonCharacterControl.java
+++ src/mygame/ThirdPersonCharacterControl.java
@@ -6,43 +6,51 @@ import com.jme3.animation.LoopMode;
 import com.jme3.bullet.control.BetterCharacterControl;
 import com.jme3.input.InputManager;
 import com.jme3.input.KeyInput;
-import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
-import com.jme3.input.controls.MouseAxisTrigger;
-import com.jme3.math.FastMath;
-import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
-import com.jme3.scene.CameraNode;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.control.CameraControl;
 import java.util.HashMap;
 
 /**
- * A class to control a third person character.
- * @author ssuri
+ * A class to control the third person character.
+ * Although some aspects of this class are specific to this game, I have
+ * tried to create it so that it would not be difficult to port to a
+ * different game.
+ * @author Sameer Suri
  */
 public class ThirdPersonCharacterControl extends BetterCharacterControl
-                                         implements ActionListener, AnalogListener
+                                         implements ActionListener
 {
+    // Booleans tracking which direction the player is moving
+    // true -> "yes, the player is moving in that direction"
+    // false -> "no, the player is not moving in that direction"
+    // Multiple of these can be true at any given time.
     private boolean forward, backward, left, right;
-    private final Node head;
-    private float yaw = 0;
-    
+
+    // Constants describing the movement of the character
     private static final float moveSpeed = 10f, sensitivity = 15f, jumpBoost = 2;
     
+    // Constants describing the physical characteristics of the character
     private static final float _radius = .5f, _height = 1f, _mass = 1f;
     
-    private static final float cameraFollowDistance = 3.5f;
+    // The instance of the JME camera class that we use to find out which way the player is looking.
+    private Camera cam;
 
+    // A field for mapping Strings to animation channels. This is useful because
+    // the Mixamo tool generates a number of different animation controls
+    // and animation channels for different parts of the body (see the bodyNodes
+    // constant). Due to this, it is easiest to just abstract it a bit, and map
+    // each channel to a name.
     private HashMap<String, AnimChannel> animChannels = new HashMap<>();
-    public HashMap<String, String> animations;
+    // A field for mapping names to the names of animations (which may vary 
+    // from model to model) This way, it is easy to add new animations; all you 
+    // have to do is add a new entry to the Map.
+    private HashMap<String, String> animations;
     
-    public Camera cam;
-
+    // The array that stores the names that Mixamo uses for each body part.
     private static final String[] bodyNodes = new String[]
     {
         "Beards",
@@ -56,274 +64,299 @@ public class ThirdPersonCharacterControl extends BetterCharacterControl
         "Tops"
     };
     
+    /**
+     * Constructor for the control.
+     * @param man The input manager, so the control can map certain keypresses to movement
+     * @param animations The map of animations, so the control knows which ones to use
+     * @param spatial The model used, so that the control can find the animations in the model
+     * @param cam The camera, so that the control can see where the player is looking
+     */
     public ThirdPersonCharacterControl(InputManager man,
-            HashMap<String, String> animations, Spatial spatial)
+            HashMap<String, String> animations, Spatial spatial, Camera cam)
     {
+        // Calls the BetterCharacterControl constructor, which, among other things
+        // creates a Bullet Physics entity to the player, given a radius, height,
+        // and mass (this would be represented as a capsule shape)
         super(_radius, _height, _mass);
-        head = new Node();
-        head.setLocalTranslation(0, 1.8f, 0f);
+        
+        // Initializes the keypresses. Given its own method for readability.
         initKeys(man);
+        
+        // Increases the jump force depending on how much it is boosted in the
+        // corresponding constant.
         this.setJumpForce(this.getJumpForce().mult(jumpBoost));
+        
+        // Stores the animations map in an instance variable
         this.animations = animations;
         
+        // This loops over each of the body parts that are created with the
+        // Mixamo FUSE tool for creating characters
         for(String bodyNode : bodyNodes)
         {
+            // Gets the Spatial corresponding to that String
             Spatial bodyPart = ((Node) spatial).getChild(bodyNode);
+            // Finds the animation controller put in by Mixamo
             AnimControl control = bodyPart.getControl(AnimControl.class);
             
-            switch(bodyNode)
-            {
-                case "Bottoms":
-                    int numBones = control.getSkeleton().getBoneCount();
-                    for(int i = 0; i < numBones; i++)
-                    {
-                        System.out.println("Bone: " + control.getSkeleton().getBone(i).getName());
-                    }
-                    //break;
-                default:
-                    animChannels.put(bodyNode, control.createChannel());
-                    break;
-                    
-            }
-          
+            // Creates a new animation channel so that we can use it to set
+            // animations later, and stores it in our HashMap
+            animChannels.put(bodyNode, control.createChannel());
         }
         
+        // Stores the camera in an instance variable
+        this.cam = cam;
+        
+        // Sets the current animation to the idle animation
         setAnim("Idle");
     }
     
    private void initKeys(InputManager inputManager)
     {
         // Binds keys to their respective actions
-        // For example, there is movement, jumping and turning
-
-        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
-        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        // For now, there is movement and jumping
+        
+        // This one binds forward movement to the W key
         inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_W));
+        // This one binds left movement to the A key
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        // This one binds backward movement to the S key
         inputManager.addMapping("Backward", new KeyTrigger(KeyInput.KEY_S));
+        // This one binds right movement to the D key
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        
+        // This one binds jumping to the spacebar
         inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
-        inputManager.addMapping("LookLeft",
-                new MouseAxisTrigger(MouseInput.AXIS_X, true));
-        inputManager.addMapping("LookRight",
-                new MouseAxisTrigger(MouseInput.AXIS_X, false));
-        inputManager.addMapping("LookDown",
-                new MouseAxisTrigger(MouseInput.AXIS_Y, true));
-        inputManager.addMapping("LookUp",
-                new MouseAxisTrigger(MouseInput.AXIS_Y, false));
-        inputManager.addMapping("Camera", new KeyTrigger(KeyInput.KEY_C));
 
         // Adds listeners for the action
         // This allows 'this' object to be notified when one of the above set
         // keys is pressed
-        inputManager.addListener(this, "Left");
-        inputManager.addListener(this, "Right");
         inputManager.addListener(this, "Forward");
+        inputManager.addListener(this, "Left");
         inputManager.addListener(this, "Backward");
+        inputManager.addListener(this, "Right");
         inputManager.addListener(this, "Jump");
-        inputManager.addListener(this, "LookLeft");
-        inputManager.addListener(this, "LookRight");
-        inputManager.addListener(this, "LookDown");
-        inputManager.addListener(this, "LookUp");
-        inputManager.addListener(this, "Camera");
     }
    
+   /**
+    * Notifies the character controller when a button event occurs.
+    * @param action The name of the button event
+    * @param isPressed Was it a press (true) or a release (false)?
+    * @param tpf Time since the last frame
+    */
     @Override
     public void onAction(String action, boolean isPressed, float tpf) 
     {
         switch (action)
         {
+            // If it is a movement event, update the corresponding boolean
+            case "Forward":
+                forward = isPressed;
+                break;
             case "Left":
                 left = isPressed;
+                break;
+            case "Backward":
+                backward = isPressed; 
                 break;
             case "Right":
                 right = isPressed;
                 break;
-            case "Forward":
-                forward = isPressed;
-                break;
-            case "Backward":
-                backward = isPressed;
-                break;
+            // If it's a jump event, jump,.
             case "Jump":
                 jump();
                 break;
+            // If it's a duck event (not yet implemented), duck.
             case "Duck":
                 setDucked(isPressed);
                 break;
+            /* Note: jump and duck are methods created in the superclass */
         }
     }
     
-    @Override
-    public void onAnalog(String name, float value, float tpf)
-    {
-        switch (name)
-        {
-            case "LookLeft":
-                rotate(tpf * value * sensitivity);
-                break;
-            case "LookRight":
-                rotate(-tpf * value * sensitivity);
-                break;
-            case "LookUp":
-                lookUpDown(-value * tpf * sensitivity);
-                break;
-            case "LookDown":
-                lookUpDown(value * tpf * sensitivity);
-                break;
-        }
-    }
-    
-    private void rotate(float value)
-    {
-        Quaternion rotate = new 
-        Quaternion().fromAngleAxis(FastMath.PI * value, Vector3f.UNIT_Y);
-        rotate.multLocal(viewDirection);
-        setViewDirection(viewDirection);
-    }
-    
-    private void lookUpDown(float value)
-    {
-        yaw += value;
-        yaw = FastMath.clamp(yaw, -FastMath.HALF_PI, FastMath.HALF_PI);
-        head.setLocalRotation(new Quaternion().fromAngles(yaw, 0, 0));
-    }
-    
+    // Contains the direction that is currently 'forward', for the character
     Vector3f forwardVector = new Vector3f();
     
+    /**
+     * Handles movement as the game goes on. 
+     * @param tpf Time since the last update event.
+     */
     @Override
     public void update(float tpf)
     {
+        // Has the superclass take care of some stuff
         super.update(tpf);
+        
+        // Gets the current direction of the CAMERA
         Vector3f modelForwardDir = cam.getRotation().mult(Vector3f.UNIT_Z).multLocal(1, 0, 1);
         Vector3f modelLeftDir = cam.getRotation().mult(Vector3f.UNIT_X);
         
+        // walkDirection keeps track of the direction that the player walks in
         walkDirection.set(0, 0, 0);
         if (forward)
         {
+            // If it is going forward, go in the forward direction (times the speed)
             walkDirection.addLocal(modelForwardDir.mult(moveSpeed));
             
+            // Then set the current forward direction as that.
             forwardVector.set(0, 0, 0);
             forwardVector.addLocal(walkDirection);
          }
         else if (backward)
         {
+            // If it is going backward, go in the negative forward direction (times the speed)
             walkDirection.addLocal(modelForwardDir.negate().multLocal(moveSpeed));
             
+            // Then set the current forward direction as that.
             forwardVector.set(0, 0, 0);
             forwardVector.addLocal(walkDirection);
         }   
         if (left)
         {
+            // If it is going left, go in the left direction (times the speed)
             walkDirection.addLocal(modelLeftDir.mult(moveSpeed));
             
+            // Then set the current forward direction as that.
             forwardVector.set(0, 0, 0);
             forwardVector.addLocal(walkDirection);
         }
         else if (right)
         {
+            // If it is going right, go in the negative left direction (times the speed)
             walkDirection.addLocal(modelLeftDir.negate().multLocal(moveSpeed));
             
+            // Then set the current forward direction as that.
             forwardVector.set(0, 0, 0);
             forwardVector.addLocal(walkDirection);
         }
         
+        // Handle the movement animations
         handleAnimations();
-        
+
+        // Set the current direction that we are looking at as the "forward" direction
         viewDirection.set(forwardVector);
+        // Does the same thing for the physics engine
         setViewDirection(forwardVector);
     }
     
-    
+    /**
+     * Handles animations on the player
+     * To be called in an update loop (from the update method)
+     */
     private void handleAnimations()
     {
+        // If we are moving...
         if(forward || backward || left || right)
         {
+            // ... but we are not currently in the moving animation...
             if(!getAnim().equals("Move"))
             {
+                // ... switch to the moving animation.
                 setAnim("Move");
             }
         }
+        // On the other hand, if we are not moving...
         else
         {
-            if(!getAnim("Bottoms").equals("Idle"))
+            // ... but we are not currently in the idle animation...
+            if(!getAnim().equals("Idle"))
             {
+                // ... switch to the idle animation.
                 setAnim("Idle");
             }
         }
     }
+
+    // A few different overloads of a method that do the same thing:
+    // set the current animation.
     
-    @Override
-    public void setSpatial(Spatial spatial)
-    {
-        super.setSpatial(spatial);
-        if(spatial instanceof Node)
-        {
-            ((Node) spatial).attachChild(head);
-        }
-    }
-    
-    public void setCamera(Camera cam)
-    {
-        CameraNode camNode = new CameraNode("CamNode", cam);
-        camNode.setControlDir(CameraControl.ControlDirection.SpatialToCamera);
-        head.attachChild(camNode);
-        
-        this.cam = cam;
-        
-        // Third Person Stuff
-        camNode.setLocalTranslation(new Vector3f(0, cameraFollowDistance, -cameraFollowDistance));
-        camNode.lookAt(head.getLocalTranslation(), Vector3f.UNIT_Y);
-    }
-    
+    /**
+     * Sets the current animation.
+     * The loop mode will stay the same, and the animation will be applied
+     * to all channels.
+     * @param anim The animation to switch to
+     */
     private void setAnim(String anim)
     {
-        setAnim(anim, null, animChannels.keySet().toArray(new String[0]));
+        setAnim(anim, (LoopMode) null);
     }
     
+    /**
+     * Sets the current animation.
+     * The loop mode will stay the same.
+     * @param anim The animation to switch to
+     * @param channels The animation channels to apply this to
+     */
     private void setAnim(String anim, String... channels)
     {
         setAnim(anim, null, channels);
     }
     
-    private void setAnim(String anim, LoopMode loopMode)
-    {
-        setAnim(anim, loopMode, animChannels.keySet().toArray(new String[0]));
-    }
-    
+    /**
+     * Sets the current animation.
+     * @param anim The animation to switch to.
+     * @param loopMode The loop mode (i.e. loop, not loop).
+     * @param channels The animation channels to apply this to. This argument is optional, and if not passed, will automatically assume ALL channels.
+     */
     private void setAnim(String anim, LoopMode loopMode, String... channels)
     {
+        // If the length of the channels array is 0 (the parameter was not passed)...
         if(channels.length == 0)
         {
+            // ... set it as ALL of the animations.
             channels = animChannels.keySet().toArray(new String[0]);
         }
-        
+        // For each animation channel [name]
         for(String channel : channels)
         {
+            // Get the AnimChannel object corresponding to the name
             animChannels.get(channel).setAnim(animations.get(anim));
+            // If the loopmode is null, don't change the loop mode
             if(loopMode != null)
             {
+                // If it isn't null, change it to whatever is passed.
                 animChannels.get(channel).setLoopMode(loopMode);
             }
         }
     }
     
+    /**
+     * Gets the current animation.
+     * It does this by checking the current animation on the first element of
+     * the animation channel, and is not necessarily accurate for all channels
+     * @return The name of the current animation
+     */
     private String getAnim()
     {
+        // Calls the other getAnim method with the first value in the
+        // set of all animation channels.
         return getAnim(animChannels.keySet().iterator().next());
     }
+    
+    /**
+     * Gets the current animation for a given animation channel.
+     * @param channel The name of the animation channel to check.
+     * @return The name of the current animation
+     */
     private String getAnim(String channel)
     {
+        // Gets the name (as registered in the model) of the current animation
         String anim = animChannels.get(channel).getAnimationName();
+        // Creates a variable to store the corresponding key (the name of the animation in our naming system)
         String animKey = anim;
-        
+       
+        // For each possible animation key
         for(String key : animations.keySet())
         {
+            // If its corresponding animation matches the one that is currently running
             if(anim.equals(animations.get(key)))
             {
+                // Set that as the correct key; break out of the for loop
                 animKey = key;
                 break;
             }
         }
-            
+        
+        // Return the animation key provided to you
         return animKey;
     }
 }
